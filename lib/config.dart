6eb8811cea5aa0e7087c692248dcd526e974135f
Taib-Ali-Alameri <taibbnali123@gmail.com>
2022-06-13 19:16:52 +0300

    add models

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  static const String appName = "Shopping App";
+  static const String apiURL = '10.4.197.1:8080'; //PROD_URL
+  static const loginAPI = "/user/login";
+  static const registerAPI = "/user/createAccount";
+  static const userProfileAPI = "/users/user-Profile";
+}
